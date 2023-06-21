98580490ebbc677a71a20bc7de3b3f5cf9a81c81
Mary <2567984254@qq.com>
2023-06-21 21:01:04 +0800

    2.5.3 vector容器嵌套容器

---
+++ 2.STL入门/2.5 容器算法迭代器/3_vector容器嵌套容器.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/********************************
+ * 2.5.3 vector容器嵌套容器
+ ******************************/
+
+void test1()
+{
+    vector<vector<int>> v;
+
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3;
+    vector<int> v4;
+
+    for (int i = 0; i < 4; i++)
+    {
+        v1.push_back(i + 1);
+        v2.push_back(i + 2);
+        v3.push_back(i + 3);
+        v4.push_back(i + 4);
+    }
+
+    // 将容器元素插入到vector中
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+    v.push_back(v4);
+
+    for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); it++)
+    {
+        for (vector<int>::iterator vit = (*it).begin(); vit != (*it).end(); vit++)
+        {
+            cout << *vit << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    test1();
+    return 0;
+}
